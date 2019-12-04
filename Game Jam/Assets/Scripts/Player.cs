f66f049abc565c67cb64a6569c686e9078da2393
rocfrank <rocfrank@iu.edu>
2019-12-04 17:20:32 -0500

    SAMs AA rounds and player now all move

--- Game Jam/Assets/Scripts/Player.cs
+++ Game Jam/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject missile;
+    public GameObject aabullet;
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,21 +16,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire2"))
+        {
+            FireSAM();
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireAAGun();
+        }
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.back);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
     }
 
     void FireAAGun()
     {
-
+        Instantiate(aabullet, transform.position, transform.rotation);
     }
 
     void FireSAM()
     {
-
+        Instantiate(missile, transform.position, transform.rotation);
     }
 
     void FireSAW()
     {
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 }
